dcbf026ac8645d0f823c25db96cf44df571b1ad3
PhamQuocTien <131446472+LateNight229@users.noreply.github.com>
2026-02-08 13:53:25 +0700

    [Popup_Damage]-> Update config

--- Assets/Scripts/Runtime/Features/Player/Attack/DamagePopup.cs
+++ Assets/Scripts/Runtime/Features/Player/Attack/DamagePopup.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class DamagePopup : MonoBehaviour
 {
     [SerializeField] TMP_Text text;
-    [SerializeField] float life = 0.6f;
-    [SerializeField] float rise = 0.8f;
+    [SerializeField] float life = 1f;
+    [SerializeField] float rise = 2f;
 
     Color _startColor;
     Vector3 _startPos;
 
     void Awake()
     {
-        if (!text) text = GetComponentInChildren<TMP_Text>();
+        if (!text) text = GetComponent<TMP_Text>();
         _startColor = text.color;
+
+        MeshRenderer mr = text.GetComponent<MeshRenderer>();
+        if (mr != null)
+        {
+            mr.sortingLayerName = "UI";   // hoặc "Foreground" tùy bạn có layer nào
+            mr.sortingOrder = 1000;       // số càng cao càng nổi
+        }
     }
 
     public void Play(int amount, Vector3 worldPos)
@@ -41,7 +48,7 @@ public class DamagePopup : MonoBehaviour
             transform.position = _startPos + Vector3.up * (rise * k);
 
             var c = text.color;
-            c.a = Mathf.Lerp(1f, 0f, k);
+            c.a = Mathf.Lerp(1f, 0.6f, k);
             text.color = c;
 
             yield return null;
